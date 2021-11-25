af0a870af98cc89a7017ecff53e602326592d82a
Fábio Santos Gomes <73297075+ffabiosantosgcm@users.noreply.github.com>
2021-11-24 22:54:04 -0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "google" {
+  credentials = "${file("sound-groove-333122-e550405d0670.json")}"
+  project = "sound-groove-333122"
+  region  = "southamerica-east1"
+  zone    = "southamerica-east1-a"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "vm-debian"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+ network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+    access_config {
+    }
+  }
+}
